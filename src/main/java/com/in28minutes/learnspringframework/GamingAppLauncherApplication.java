aa97ae05ce1d27875c2a278a1867e2204ce4551b
WesBourkeMusic <wesbourkeavl@gmail.com>
2023-02-05 23:53:33 -0500

    working in CLI git. test

--- src/main/java/com/in28minutes/learnspringframework/GamingAppLauncherApplication.java
+++ src/main/java/com/in28minutes/learnspringframework/GamingAppLauncherApplication.java
@@ -2,7 +2,6 @@ package com.in28minutes.learnspringframework;
 
 import com.in28minutes.learnspringframework.game.GameRunner;
 import com.in28minutes.learnspringframework.game.PackManGame;
-import com.in28minutes.learnspringframework.game.SuperContraGame;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -15,8 +14,7 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan("com.in28minutes.learnspringframework.game")
-public class App02GamingSpringBeans {
-
+public class GamingAppLauncherApplication {
     @Bean
     public GameRunner gameRunner(PackManGame game) {
         System.out.println("This is what needs to print out " + game);
@@ -24,11 +22,10 @@ public class App02GamingSpringBeans {
         return gameRunner;
     }
 
-
     public static void main(String[] args) {
 
         try(var context = new AnnotationConfigApplicationContext
-                (App02GamingSpringBeans.class);) {
+                (GamingAppLauncherApplication.class);) {
             context.getBean(PackManGame.class).up();
             context.getBean(GameRunner.class).run();
         }
